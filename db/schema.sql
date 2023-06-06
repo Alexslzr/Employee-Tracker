0d08c68aa5b549569784fab7b3d3570acd11cff7
Alejandro Salazar <alejandroslzr17@gmail.com>
2023-06-06 14:19:21 -0600

    created the schema.sql, and the seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -3,20 +3,20 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE department(
+CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role(
+CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
-    FOREIGN Key (department) REFERENCES department(id)
+    FOREIGN Key (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE employee(
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO department(name)
+VALUES ("Human Resources"),
+        ("Engineering"),
+        ("Sales"),
+        ("Finance");
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ("SalesPerson",5000,3),
+        ("Sales Lead",7500,3),
+        ("HR",3000,1),
+        ("HR Lead",5000,1),
+        ("Software Engineer", 7500,2),
+        ("Tech Lead", 10000,2),
+        ("Finance Person", 6000,4),
+        ("Finance Lead", 8000,4);
+
+INSERT INTO employee(first_name,last_name,role_id,manager_id)
+VALUES  ("Alejandro", "Salazar", 6 ,NULL),
+        ("Mike", "Felix", 5 , 1 ),
+        ("Clarissa", "Jacobs", 4 ,NULL),
+        ("Louis", "Brown", 3 , 3 ),
+        ("Graciela", "Caro", 8 , NULL),
+        ("Carlos", "Quintero", 7 , 5),
+        ("Joel", "Guzman", 2 , NULL),
+        ("Fernanda", "Lopez", 1 , 7 );
+        
+        
+        
+        
